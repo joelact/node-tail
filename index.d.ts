321060fe83ba3c64b4c8a963ee8bc007e2c87a97
Joel Teixeira <joelateixeira7@gmail.com>
2022-12-03 00:59:47 +0000

    Fixed types

--- index.d.ts
+++ index.d.ts
@@ -1,24 +1,21 @@
-declare module 'node-tail' {
-    export type Options = {
-        /**
-         * Encoding of the file (default: utf8)
-         */
-        encoding: string;
-    };
+export type Options = {
+    /**
+     * Encoding of the file (default: utf8)
+     */
+    encoding: string;
+};
 
-    export default class Tail {
-        /**
-         *
-         * @param filename Path to the file to tail
-         * @param options Options for reading the file
-         */
-        constructor(filename: string, options: Options);
-
-        /**
-         * Returns n lines from the file passed to the constructor
-         *
-         * @param n Number of lines to return from the file (default: 10)
-         */
-        readLines(n?: number): Promise<string[]>;
-    }
+export default class Tail {
+    /**
+     *
+     * @param filename Path to the file to tail
+     * @param options Options for reading the file
+     */
+    constructor(filename: string, options: Options);
+    /**
+     * Returns n lines from the file passed to the constructor
+     *
+     * @param n Number of lines to return from the file (default: 10)
+     */
+    readLines(n?: number): Promise<string[]>;
 }
